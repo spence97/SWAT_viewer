f023cf171381c2b20a81b8a66be48ed065839d9f
Spencer McDonald <spencermcdonald11@gmail.com>
2018-06-16 16:46:02 +0700

    fix file download code

--- tethysapp/swat/config.py
+++ tethysapp/swat/config.py
@@ -5,3 +5,7 @@ temp_workspace = os.path.join('/home/ubuntu/tethys_temp/swat/')
 
 data_path = os.path.join('/home/ubuntu/swat_data/')
 
+# temp_workspace = os.path.join('/Users/Student/Documents/tethys_temp_files/swat')
+#
+# data_path = os.path.join('/Users/Student/Documents/tethysdev/swat_data')
+
