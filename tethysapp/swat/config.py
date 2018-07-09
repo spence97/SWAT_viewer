b1e537dab59d46d421c734143c51beae1bae66d5
spence97 <spencermcdonald11@gmail.com>
2018-07-08 21:31:17 -0700

    change file paths to adpc server

--- tethysapp/swat/config.py
+++ tethysapp/swat/config.py
@@ -1,16 +1,16 @@
 import os
 
 
-# temp_workspace = os.path.join('/home/ubuntu/tethys_temp/swat/')
-#
-# data_path = os.path.join('/home/ubuntu/swat_data/')
+temp_workspace = os.path.join('/home/ubuntu/tethys_temp/swat/')
+
+data_path = os.path.join('/home/ubuntu/swat_data/')
 
 # temp_workspace = os.path.join('/Users/Student/Documents/tethys_temp_files/swat')
 #
 # data_path = os.path.join('/Users/Student/Documents/tethysdev/swat_data')
 
-temp_workspace = os.path.join('/home/spencer/Documents/tethys_temp/swat')
-
-data_path = os.path.join('/home/spencer/Documents/swat_data')
+# temp_workspace = os.path.join('/home/spencer/Documents/tethys_temp/swat')
+#
+# data_path = os.path.join('/home/spencer/Documents/swat_data')
 
 watershed_xml_path = os.path.join('SWAT_viewer/tethysapp/swat/public/watershed_data/watershed_info.xml')
